2048891045dadc1cfa9415d70eeb87339b02cc08
Daris Noreme <84494030+noreme@users.noreply.github.com>
2025-06-03 13:52:15 +0200

    Update

--- js/init.js
+++ js/init.js
@@ -13,7 +13,7 @@
     setTimeout(function() {
 	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
 	 }, 100);
-
+ 
 
 /*----------------------------------------------------*/
 /* Smooth Scrolling
